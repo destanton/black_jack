903b52b9b15d426c12d0840891a8c3cbe8d05aa3
destanton <delisestanton@gmail.com>
2016-09-22 17:24:28 -0400

    added changes to deck shuffle

--- deck_and_card.py
+++ deck_and_card.py
@@ -1,7 +1,5 @@
 from random import choice
 from random import shuffle
-rank = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-suit = ["Diamond", "Spades", "Club", "Heart"]
 
 
 class Player:
@@ -12,31 +10,45 @@ class Player:
 class Card:
 
     def __init__(self, rank, suit):
-        self.rank = choice(rank)
-        self.suit = choice(suit)
+        self.rank = rank
+        self.suit = suit
         # jack, king, queen is a 10
         # ace is 11
 
 
-# class Deck:
-#     def __init__(self, card, rank, suit):
-#         self.deck = []
-#
-#         for rank in rank:
-#             for suit in suit:
-#                 deck.append(rank, suit)
-#
-#     def shuffle_deck(self):
-#         self.shuffle = shuffle_deck
-        # self.deal = # need to deal 1 card at a time to dealer and player
+class Deck:
+    def __init__(self):
+        ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+        suits = ["Diamonds", "Spades", "Clubs", "Hearts"]
+        self.all_cards = []
 
-    # for x in range(52)
-    # card()
+        for rank in ranks:
+            for suit in suits:
+                self.all_cards.append(Card(rank, suit))
+
+    def show_card(self):
+
+        for card in self.all_cards:
+            print(card.rank, card.suit)
+
+    def shuffle_deck(self):
+        shuffle(self.all_cards)
+
+        # self.deal =  # need to deal 1 card at a time to dealer and player
+
+        # for x in range(52)
+        # card()
 
 user = Player("Danielle")
 dealer = Player("Dealer")
-card = Card(rank, suit)
-
+deck = Deck()
+deck.shuffle_deck()
+# print(deck.all_cards)
+# card = Card(rank, suit)
 print(user.name)
 print(dealer.name)
-print(card.rank, card.suit)
+# print(card.rank, card.suit)
+print(deck.show_card())
+# print(deck.deck)
+
+# print(deck.deck)
