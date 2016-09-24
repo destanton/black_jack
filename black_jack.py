e4b187d674f1c0b23e3a48ed678547a5fc16b62b
destanton <delisestanton@gmail.com>
2016-09-23 20:27:25 -0400

    player and dealer turns working

--- black_jack.py
+++ black_jack.py
@@ -25,22 +25,32 @@ class Game:
         #     dealer_hand.show()
 
         while True:
-            if user_hand.get_value() < 21 and dealer_hand.get_value() < 17:
-                print("Dealer draws another card")
-                dealer_hand.add_card(deck.deal_card())
-                print(dealer_hand.show())
-                print(dealer_hand.get_value())
-                # game.hit_or_stand()
-            elif user_hand.get_value() < 21 and dealer_hand.get_value() < 21:
-                dealer_hand.add_card(deck.deal_card())
-                print(dealer_hand.get_value())
-            elif dealer_hand.get_value() >= 21:
-                print("Dealer Busts. Player wins!")
+            if user_hand.get_value() == 21:
+                print("BLACKJACK PLAYER WINS!!!")
+                return False
+            elif dealer_hand.get_value() == 21:
+                print("BLACKJACK DEALER WINS!!!")
+                return False
+            elif user_hand.get_value() < 16:
+                game.hit_or_stand()
+                print(user_hand.get_value())
                 return False
             elif user_hand.get_value() >= 21:
                 print("Game Over! You Busted!")
                 return False
-            elif user_hand.get_value() > dealer_hand.get_value():
+            elif user_hand.get_value() >= 16 and dealer_hand.get_value() < 17:
+                print("Dealer draws another card")
+                dealer_hand.add_card(deck.deal_card())
+                print(dealer_hand.get_value())
+                # print("Dealer draws another card")
+                # print("\n")
+                # dealer_hand.add_card(deck.deal_card())
+                # print(dealer_hand.show())
+                # print(dealer_hand.get_value())
+            elif dealer_hand.get_value() >= 21:
+                print("Dealer Busts. Player wins!")
+                return False
+            elif user_hand.get_value() >= dealer_hand.get_value():
                 print("Game Over! Player's hand beats the Dealer!")
                 return False
             else:
