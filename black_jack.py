b25625a30db236284905edcec7dad4a789c184b9
destanton <delisestanton@gmail.com>
2016-09-25 11:37:56 -0400

    updated game play loop

--- black_jack.py
+++ black_jack.py
@@ -2,6 +2,7 @@ from deck_and_card import Deck, Card, Hand
 from players import Player
 from players import Dealer
 import sys
+import random
 
 
 class Game:
@@ -10,56 +11,84 @@ class Game:
                        Dealer()]
         self.deck = deck
 
+    def check_blackjack(self):
+        while True:
+            if user_hand.get_value() == 21:
+                print("BLACKJACK PLAYER WINS!!!")
+                sys.exit()
+            elif dealer_hand.get_value() == 21:
+                print("BLACKJACK DEALER WINS!!!")
+                sys.exit()
+            else:
+                return False
+
     def hit_or_stand(self):
         choice = input("Do you want to [H]it or [S]tand? ").lower()
         if choice == "h":
             user_hand.add_card(deck.deal_card())
             user_hand.show()
-            print(user_hand.get_value())
+            print("\n")
+            print("Your new total is {} ".format(user_hand.get_value()))
         else:
-            print("Player's hand:")
-            print(user_hand.get_value())
-            print("Dealer's hand:")
-            print(dealer_hand.get_value())
+
+            game.check_winner()
+    
+
+
         #     dealer_hand.add_card(deck.deal_card())
         #     dealer_hand.show()
 
-        while True:
-            if user_hand.get_value() == 21:
-                print("BLACKJACK PLAYER WINS!!!")
-                return False
-            elif dealer_hand.get_value() == 21:
-                print("BLACKJACK DEALER WINS!!!")
-                return False
-            elif user_hand.get_value() < 16:
-                game.hit_or_stand()
-                print(user_hand.get_value())
-                return False
-            elif user_hand.get_value() >= 21:
-                print("Game Over! You Busted!")
-                return False
-            elif user_hand.get_value() >= 16 and dealer_hand.get_value() < 17:
-                print("Dealer draws another card")
+    def check_winner(self):
+        # while True:
+            if user_hand.get_value() <= 21 and dealer_hand.get_value() != 21:
                 dealer_hand.add_card(deck.deal_card())
+                print("Dealer's hand")
                 print(dealer_hand.get_value())
-                # print("Dealer draws another card")
-                # print("\n")
-                # dealer_hand.add_card(deck.deal_card())
-                # print(dealer_hand.show())
-                # print(dealer_hand.get_value())
-            elif dealer_hand.get_value() >= 21:
-                print("Dealer Busts. Player wins!")
-                return False
-            elif user_hand.get_value() >= dealer_hand.get_value():
-                print("Game Over! Player's hand beats the Dealer!")
-                return False
+                # game.hit_or_stand()
+                # return False
+            elif dealer_hand.get_value() == 21 and user_hand.get_value() < 21:
+                print("Dealer WINS")
+                # return False
+            elif dealer_hand.get_value() > 21 and user_hand.get_value() < 21:
+                print("You win! Dealer went over 21")
+                # return False
+            elif user_hand.get_value() > 21 and dealer_hand.get_value() <= 21:
+                print ("You lose! You went over 21")
+                # return False
             else:
-                break    # print(choice)
+                print("Your hand is over 21, you lose")
+                print("\n")
+                # return False
+
+            # elif user_hand.get_value() < 16:
+            #     game.hit_or_stand()
+            #     print(user_hand.get_value())
+            #     return False
+            # elif user_hand.get_value() >= 21:
+            #     print("Game Over! You Busted!")
+            #     return False
+            # elif user_hand.get_value() >= 16 and dealer_hand.get_value() < 17:
+            #     print("Dealer draws another card")
+            #     dealer_hand.add_card(deck.deal_card())
+            #     print(dealer_hand.get_value())
+            #     # print("Dealer draws another card")
+            #     # print("\n")
+            #     # dealer_hand.add_card(deck.deal_card())
+            #     # print(dealer_hand.show())
+            #     # print(dealer_hand.get_value())
+            # elif dealer_hand.get_value() >= 21:
+            #     print("Dealer Busts. Player wins!")
+            #     return False
+            # elif user_hand.get_value() >= dealer_hand.get_value():
+            #     print("Game Over! Player's hand beats the Dealer!")
+            #     return False
+            # else:
+            #     break    # print(choice)
 
     def replay(self):
         choice = input("Do you want to play again? [Y]es/n ").lower()
         if choice != "n":
-            game()
+            Game()
         else:
             print("Bye!")
             sys.exit
@@ -100,6 +129,9 @@ game = Game()
 #         break
 
 game = Game()
+game.check_blackjack()
 game.hit_or_stand()
+game.check_winner()
+game.replay = Game()
 
 # turns []
