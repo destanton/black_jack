72ee7b6b4a6722f39a5f394919b5e3a95e5564be
destanton <delisestanton@gmail.com>
2016-09-25 13:21:38 -0400

    cleaning up interface

--- black_jack.py
+++ black_jack.py
@@ -19,7 +19,10 @@ class Game:
                 print("BLACKJACK DEALER WINS!!!")
                 sys.exit()
             elif user_hand.get_value() > 21:
-                print("You Busted!")
+                print("You Busted! ¯\_(ツ)_/¯ ")
+                sys.exit()
+            elif dealer_hand.get_value() > 21:
+                print("You Busted! ¯\_(ツ)_/¯ ")
                 sys.exit()
             else:
                 return False
@@ -31,30 +34,42 @@ class Game:
             user_hand.show()
             print("\n")
             print("Your new total is {} ".format(user_hand.get_value()))
+            print("\n")
             game.player_turn()
         else:
-            print("Dealer Draws...")
+
             game.check_dealer()
 
     def player_turn(self):
         high_range = 21
-        low_range = 17
+        low_range = 20
         while True:
             if user_hand.get_value() >= high_range:
                 game.check_winner()
             elif user_hand.get_value() <= low_range:
                 game.hit_or_stand()
+            else:
+                game.check_dealer()
 
     def check_dealer(self):
         high_range = 21
-        mid_range = 18
-        low_range = 17
+        low_range = 16
 
         while True:
-            if dealer_hand.get_value() <= mid_range:
+            if dealer_hand.get_value() <= low_range:
+                print("Dealer Draws...")
+                print("\n")
                 dealer_hand.add_card(deck.deal_card())
                 print("Dealer's hand:")
-                # dealer_hand.show()
+                print(dealer_hand.show())
+                print(dealer_hand.get_value())
+                print("\n")
+            elif dealer_hand.get_value() == user_hand.get_value():
+                print("Dealer Draws...")
+                print("\n")
+                dealer_hand.add_card(deck.deal_card())
+                print("Dealer's hand:")
+                print(dealer_hand.show())
                 print(dealer_hand.get_value())
             else:
                 game.check_winner()
@@ -70,7 +85,7 @@ class Game:
             # elif user_hand.get_value() < high_range:
             #     game.hit_or_stand()
             elif user_hand.get_value() > high_range:
-                print ("You lose! You Busted")
+                print ("You lose! You Busted! ¯\_(ツ)_/¯")
                 sys.exit()
             elif user_hand.get_value() == dealer_hand.get_value():
                 game.check_dealer()
@@ -109,20 +124,6 @@ dealer_hand.show()
 print("\n")
 # print(user_hand.get_value())
 # print(dealer_hand.get_value())
-game = Game()
-# while True:
-#     if user_hand.get_value() < 21:
-#         game.hit_or_stand()
-#     elif user_hand.get_value() > 21:
-#         print("Game Over! You Busted!")
-#         break
-#     # elif dealer_hand.get_value() < 16:
-#     #     dealer_hand.add_card(deck.deal_card())
-#     # elif dealer_hand.get_value() > 21:
-#     #     print("Dealer Busts. Player wins!")
-#     #     break
-#     else:
-#         break
 
 game = Game()
 game.check_blackjack()
