94f1be2afa96010bff47b5404818ec16648a692c
destanton <delisestanton@gmail.com>
2016-09-28 20:29:26 -0400

    cleaned up game

--- black_jack.py
+++ black_jack.py
@@ -12,13 +12,13 @@ class Game:
 
     def check_blackjack(self):
         if user_hand.get_value() == 21 or dealer_hand.get_value() == 21:
-            print("OH SNAP! BLACKJACK!!! {} is player's score & {} is dealer's score!".format(user_hand.get_value(), dealer_hand.get_value()))
+            print("OH SNAP! BLACKJACK!!! The player's score is {} & the dealer's score is {}!".format(user_hand.get_value(), dealer_hand.get_value()))
             sys.exit()
         elif user_hand.get_value() > 21:
-            print("You Busted! ¯\_(ツ)_/¯ ")
+            print("YOU BUSTED! ¯\_(ツ)_/¯ ")
             sys.exit()
         elif dealer_hand.get_value() > 21:
-            print("You Busted! ¯\_(ツ)_/¯ ")
+            print("DEALER BUSTED! ¯\_(ツ)_/¯ ")
             sys.exit()
         else:
             game.hit_or_stand()
@@ -42,67 +42,59 @@ class Game:
 
     def check_dealer(self):
         while dealer_hand.get_value() < 21:
-            if dealer_hand.get_value() < 17: #or dealer_hand.get_value() < user_hand.get_value():
+            if dealer_hand.get_value() < 17 or dealer_hand.get_value() < user_hand.get_value():
                 dealer_hand.add_card(deck.deal_card())
-                dealer_hand.show()
-                game.check_winner()
+                # dealer_hand.show()
+                # game.check_winner()
             elif dealer_hand.get_value() == user_hand.get_value():
                 dealer_hand.add_card(deck.deal_card())
                 dealer_hand.show()
                 game.check_winner()
-            else:
-                game.check_winner()
+        else:
+            dealer_hand.show()
+            game.check_winner()
 
     def check_winner(self):
         if dealer_hand.get_value() > 21:
-            print("\nYOU WIN! DEALER BUSTED!")
+            print("\nYOU WIN! DEALER BUSTED!\n")
             sys.exit()
         elif user_hand.get_value() > 21:
-            print ("\nYOU LOSE! YOU BUSTED! ¯\_(ツ)_/¯")
+            print ("\nYOU LOSE! YOU BUSTED! ¯\_(ツ)_/¯\n")
             sys.exit()
         elif user_hand.get_value() == dealer_hand.get_value():
-            print("\nIT'S A TIE! DEALER WINS!")
+            print("\nIT'S A TIE! DEALER WINS!\n")
+            sys.exit()
         elif dealer_hand.get_value() > user_hand.get_value():
-            print("\nDEALER WINS!!!")
+            print("\nDEALER WINS!!!\n")
             sys.exit()
         elif user_hand.get_value() > dealer_hand.get_value():
-            print("\nYOU WIN!")
+            print("\nYOU WIN!\n")
             sys.exit()
 
-    def replay(self):
-        choice = input("Do you want to play again? [Y]es/n ").lower()
-        if choice != "n":
-            game.replay()
-        else:
-            print("Bye!")
-            sys.exit
+# game = Game()
+while True:
+    game = Game()
+    user = Player("Danielle")
+    dealer = Player("Dealer")
+    deck = Deck()
+    deck.shuffle_deck()
+    user_hand = Hand(user)
+    dealer_hand = Hand(dealer)
 
-game = Game()
-user = Player("Danielle")
-dealer = Player("Dealer")
-deck = Deck()
-deck.shuffle_deck()
-user_hand = Hand(user)
-dealer_hand = Hand(dealer)
-
-print("{}'s hand is".format("Your"))
-user_hand.add_card(deck.deal_card())
-user_hand.add_card(deck.deal_card())
-user_hand.show()
-print("\n{}'s hand is".format("Dealer"))
-dealer_hand.add_card(deck.deal_card())
-dealer_hand.add_card(deck.deal_card())
-dealer_hand.show()
-print("\n")
-game.check_blackjack()
-game.hit_or_stand()
+    print("{}'s hand is".format("Your"))
+    user_hand.add_card(deck.deal_card())
+    user_hand.add_card(deck.deal_card())
+    user_hand.show()
+    print("\n{}'s hand is".format("Dealer"))
+    dealer_hand.add_card(deck.deal_card())
+    dealer_hand.add_card(deck.deal_card())
+    dealer_hand.show()
+    print("\n")
+    game.check_blackjack()
+    game.hit_or_stand()
 
 
-# print(user_hand.get_value())
-# print(dealer_hand.get_value())
-
-
-game.player_turn()
-game.check_dealer()
-game.check_winner()
-game.replay()
+    game.player_turn()
+    game.check_dealer()
+    game.check_winner()
+    # game.replay()
